5255d87d0eb340e12a7dcfc333dbc32f66fa60c9
Ysw <202021079@sangmyung.kr>
2024-07-21 00:32:34 +0900

    #코드 수정

--- src/main/java/me/yuseonu/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/yuseonu/springbootdeveloper/controller/BlogApiController.java
@@ -16,19 +16,20 @@ import java.util.List;
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
 public class BlogApiController {
 
-    private  final BlogService blogService;
+    private final BlogService blogService;
 
     // HTTP 메서드가 POST일 떄 전달받은 URL과 동일하면 메서드로 매핑
     @PostMapping("/api/articles")
     // @RequestBody로 요청 본문 값 매핑
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest
-                                              request){
+            request) {
         Article savedArticle = blogService.save(request);
 
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
+
     // 블로그 글 목록 조회
     @GetMapping("/api/articles")
     public ResponseEntity<List<ArticleResponse>> findAllArticles() {
@@ -40,6 +41,7 @@ public class BlogApiController {
         return ResponseEntity.ok()
                 .body(articles);
     }
+
     // 블로그 글 조회
     @GetMapping("/api/articles/{id}")
     // URL 경로에서 값 추출
@@ -49,6 +51,7 @@ public class BlogApiController {
         return ResponseEntity.ok()
                 .body(new ArticleResponse(article));
     }
+
     // 블로그 글 삭제
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
@@ -57,10 +60,11 @@ public class BlogApiController {
         return ResponseEntity.ok()
                 .build();
     }
+
     // 블로그 글 수정
     @PutMapping("/api/articles/{id}")
     public ResponseEntity<Article> updateArticle(@PathVariable long id,
-                                                 @RequestBody UpdateArticleRequest request){
+            @RequestBody UpdateArticleRequest request) {
         Article updatedArticle = blogService.update(id, request);
 
         return ResponseEntity.ok()
